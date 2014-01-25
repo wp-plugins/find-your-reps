8f4d77ac667c05e7caed240f896e76c6d21a9d44
kathleenfmalone <kathleenfmalone@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-25 01:53:22 +0000

    First version of Find Your Reps
    git-svn-id: https://plugins.svn.wordpress.org/find-your-reps/trunk@844928 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,29 @@
+=== Find Your Reps ===
+Contributors:  kathleenfmalone
+Plugin website: http://www.getontheagenda.com
+Tags: find state representatives, find state legislators, representative, legislator, state legislature, legislature
+Requires at least: 3.5.2
+Tested up to: 3.5.2
+Stable tag: 1.0
+Version: 1.0
+License: GPLv2 or later
+
+Find your state representatives by entering a street address.
+
+== Description ==
+
+Find Your Reps uses Google Maps Geocoding API to take a street address and convert it to latitude and longtitude. The latitude and
+longtitude values are sent to the openstates.org API and the user's state representatives name, photo, website, email, district office
+address and capitol office address is returned to them. 
+
+== Installation ==
+
+1. After downloading the plugin, unzip the plugin.
+2. Upload the `find-your-reps` directory into your `wp-content/plugins` directory so that the structure is wp-content/plugins/find-your-reps/....
+3. Activate the plugin in the backend of your WordPress site.
+4. Configure the plugin via the Find Your Reps link that appears in the Settings Menu section of the left sidebar in the backend.
+5. The plugin requires an activation key from the Sunlight Foundation. The key can be acquired by registering at http://sunlightfoundation.com/api/accounts/register/.  
+
+= 1.0.0 =
+
+  * Initial release
---
+++ find-your-reps.php
@@ -0,0 +1,91 @@
+<?php
+    
+/**
+ * @package Find Your State Representatives
+ * @version 1.0
+ */
+/*
+Plugin Name: Find Your Representatives
+Plugin URI: http://getontheagenda.com
+Description: This plugin finds your state representatives by street address and returns their 
+names, photos, websites, email addresses and district and capitol office addresses. The plugin uses 
+state representative data from openstates.org's API. The use of openstate.org's API requires a key 
+from the Sunlight Foundation. A key can be acquired by registering at http://sunlightfoundation.com/api/accounts/register/. 
+The key must be entered on the plugin's settings page.
+Author: Kathleen Malone
+Author Email: kathleenfmalone@gmail.com
+Version: 1.0
+Author URI: http://none
+*/
+
+if (is_admin() ) {
+    
+    require_once( plugin_dir_path(__FILE__).'/includes/admin.php');
+}
+
+function fyr_scripts_method() {
+	wp_enqueue_script(
+		'fyr_find_state_reps_script',
+		plugins_url( '/js/fyr-find-state-reps.js' , __FILE__ ), false
+	);
+
+    wp_enqueue_script('googleapis', 
+        'https://maps.googleapis.com/maps/api/js?sensor=false', false
+    );
+    wp_enqueue_script('googlegeocode', 
+    'http://maps.googleapis.com/maps/api/geocode/output?json', false 
+    );
+
+    wp_enqueue_script( 'jquery' );
+
+    fyr_define_javascript_vars();
+    
+}
+
+function fyr_define_javascript_vars() {
+
+   //this function assigns the plugin option values to variables available in the javascript file, fyr-find-state-reps.js
+   $fyr_plugin_options = get_option('fyr_plugin_options'); 
+   wp_localize_script( 'fyr_find_state_reps_script', 'fyr_plugin_options_for_javascript', $fyr_plugin_options );
+    
+}
+
+function fyr_shortcode_find_state_reps(){
+
+ ?>
+        
+<div class="wrap">
+
+    <div id="fyr_find_reps" style="float:left;">
+
+        <h3>Enter your address and we will find your state representatives. </h3><br>
+
+        <form action="#" onsubmit="return fyr_get_state_rep_data(this);"> 
+        
+        <table>
+        <tr><td>Street Address</td><td> <input id="fyr_street_address" type="text"></td></tr>
+        <tr><td>City</td><td> <input id="fyr_city" type="text"></td></tr>
+        <tr><td>State</td><td> <input id="fyr_state" type="text"></td></tr>
+        <tr><td>Zipcode</td><td> <input id="fyr_zipcode" type="text"></td></tr>
+        </table>
+
+        <input type="submit" value="Find" name="submit" />
+        </form> 
+
+    </div>
+
+     <div id="map_canvas" style="width:250px;height:250px;float:left;"></div>
+
+</div>
+        
+
+
+                <?php  
+    
+}
+
+add_action( 'wp_enqueue_scripts', 'fyr_scripts_method' );
+add_shortcode('fyr_find_state_reps', 'fyr_shortcode_find_state_reps');
+
+
+?>
