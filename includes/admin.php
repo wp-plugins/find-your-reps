70b69ed7df9d345a0b4b9f280f97bbce3ece01b6
homergz <homergz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-18 04:57:09 +0000

    Tagging 1.1
    git-svn-id: https://plugins.svn.wordpress.org/find-your-reps/tags/1.1@1183105 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/admin.php
@@ -0,0 +1,99 @@
+<?php
+
+/*Create a sub menu for Find Your Reps plugin to the Settings menu by calling the fyr_create_menu function*/
+add_action( 'admin_menu', 'fyr_create_menu');
+
+function fyr_create_menu() {
+
+/*Calls the fyr_settings function to create the Find Your Reps menu page*/
+
+add_options_page( 'Find Your Reps', 'Find Your Reps', 'manage_options', 'fyr_slug', 'fyr_settings' );
+
+}
+
+function fyr_save_settings() {
+
+$fyr_plugin_options = array(
+'fyr_sunlight_key' =>
+$_POST["fyr_sunlight_key"],
+);
+
+update_option('fyr_plugin_options', $fyr_plugin_options);
+
+?>
+<div class="wrap">
+  
+  <h2>
+    Your Settings Have Been Saved
+  </h2>
+  
+</div>
+
+<?php  
+}
+
+function fyr_settings () {
+
+if (isset($_POST['fyr_sunlight_key'])) {
+fyr_save_settings();
+return;
+}
+else {
+$fyr_options = get_option('fyr_plugin_options');
+$fyr_sunglight_key = $fyr_options['fyr_sunlight_key'];
+
+?>
+<div class="wrap">
+  <?php screen_icon ( 'plugins' ); ?>
+  <h2>
+    Find Your Representatives Settings
+  </h2>
+  <form name="fyr_save_settings" method="POST" action="" >
+    <table class="form-table">
+      <tr>
+        <td>
+          Sunlight Foundation Key:
+        </td>
+        <td>
+          <input maxlength="32" size="32" name="fyr_sunlight_key" value="<?php echo $fyr_sunglight_key;?>">
+                 <p>
+                   You can get this at the 
+                   <a href="http://sunlightfoundation.com/api/accounts/register/" >
+                     The Sunlight Foundation's Website
+                   </a>
+                 </p>
+             </td>
+             
+             </tr>
+             <tr>
+               <td>
+                 <h3>
+                   Shortcode
+                 </h3>
+               </td>
+               <td>
+               </td>
+             </tr>
+             <tr>
+               <td>
+                 Find State Representatives:
+               </td>
+               <td>
+                 [fyr_find_state_reps]
+               </td>
+             </tr>
+             
+             <tr>
+               <td>
+                 <input type="submit" name="save" value="Save Settings" class="button-primary"/>
+               </td>
+             </tr>
+             </table>
+          </form>
+          
+</div>
+
+<?php  
+}
+}
+?>
---
+++ js/fyr-find-state-reps.js
@@ -0,0 +1,116 @@
+   var fyr_latlong = new google.maps.LatLng();
+
+     function fyr_set_map() {
+     
+        var mapOptions = {
+          center: new google.maps.LatLng(fyr_latlong.lat(), fyr_latlong.lng()),
+          zoom: 13,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map_canvas"),
+            mapOptions);
+      }
+
+    function fyr_ajax_successfn(fyr_results, status, xhr) {
+              
+         var fyr_rep_info = document.getElementById('fyr_find_reps');         
+         fyr_rep_info.innerHTML = '<b>Your Representatives Are:</b><br>';
+
+         jQuery.each(fyr_results, function (index, value) {
+
+             var fyr_photoElement = document.createElement("div");
+             var fyr_nameElement = document.createElement("div");
+
+             fyr_photoElement.innerHTML = '<b><br><img src="' + this.photo_url + '" style="width:100px;height:150px;" alt="' + this.full_name + '">';
+             fyr_nameElement.innerHTML = "<br><b>" + this.full_name + "</b>";
+
+             var fyr_officesElement = document.createElement("div");
+             var fyr_nameWebsiteEmailElement = document.createElement("div");
+
+             for (var i = 0; i < this.offices.length; i++) {
+                 if (this.offices[i].name == "District Office") {
+                     fyr_officesElement.innerHTML += "<br><br><b>District Office<br></b>" + this.offices[i].address.replace(/\n/g, "<br>");
+                     if (this.offices[i].phone != null) { fyr_officesElement.innerHTML += "<br>" + this.offices[i].phone; }
+                     
+                 }
+                 else {
+                     fyr_officesElement.innerHTML += "<br><br><b>Capitol Office<br></b>" + this.offices[i].address.replace(/\n/g, "<br>");
+                     if (this.offices[i].phone != null) { fyr_officesElement.innerHTML += "<br>" + this.offices[i].phone; }
+                 }
+             }
+
+             fyr_nameWebsiteEmailElement.innerHTML += '<br><b>Website:</b> <a href = "' + this.url +'" target="_blank">'+ this.url + "</a>";
+             fyr_nameWebsiteEmailElement.innerHTML += '<br><b>Email:</b> <a href="mailto:' + this.email + '">'+this.email+"</a><br><br>";
+             fyr_rep_info.appendChild(fyr_photoElement);
+             fyr_rep_info.appendChild(fyr_nameElement);
+             fyr_rep_info.appendChild(fyr_officesElement);
+             fyr_rep_info.appendChild(fyr_nameWebsiteEmailElement);
+
+         }); 
+
+         fyr_set_map();
+    }
+
+    function fyr_ajax_errorfn(xhr, status, strErr) {
+        alert("There was an error processing your request. Please try again.");
+    }
+
+    function fyr_get_state_rep_data(f) {
+
+        fyr_street_address = document.getElementById("fyr_street_address").value;
+        if (fyr_street_address == null || fyr_street_address == "") { 
+            alert("Address Line 1 is a required field"); 
+                return false; 
+        }
+        
+        fyr_city = document.getElementById("fyr_city").value;
+        if (fyr_city == null || fyr_city == "") { 
+            alert("City is a required field"); 
+                return false; 
+        }
+                        
+        fyr_state = document.getElementById("fyr_state").value;
+        if (fyr_state == null || fyr_state == "") { 
+            alert("State is a required field"); 
+                return false; 
+        }
+                
+        fyr_zipcode = document.getElementById("fyr_zipcode").value;
+        if (fyr_zipcode == null || fyr_zipcode == "") { 
+            alert("Zipcode is a required field"); 
+                return false; 
+        }
+        
+        fyr_full_address = fyr_street_address +  ", " + fyr_city + ", " + fyr_state;
+        
+        var fyr_geocoder = new google.maps.Geocoder();
+        
+        fyr_geocoder.geocode({ 'address': fyr_full_address }, function (results, status) {
+
+            if (status == google.maps.GeocoderStatus.OK) {
+                fyr_latlong = results[0].geometry.location;
+                //  Create the Sundlight Foundation API url request        
+                var fyr_slapi_url = "http://openstates.org/api/v1/legislators/geo/?long=" + fyr_latlong.lng() + "&lat=" + fyr_latlong.lat() + "&apikey=" + fyr_plugin_options_for_javascript.fyr_sunlight_key;
+                fyr_slapi_url += "&fields=full_name,photo_url,email,url,offices";
+                               
+                // Create a jQuery ajax request to the sunlight foundation api
+                jQuery.ajax({
+                    url: fyr_slapi_url,
+                    type: "GET",
+                    dataType: "jsonp",
+                    success: fyr_ajax_successfn,
+                    error: fyr_ajax_errorfn,
+                    complete: function (xhr, status) {
+                    }
+
+                });
+
+            } else {
+                alert("We are unable to process your request..." + status);
+            }
+
+        });
+
+        return false;
+        
+    } 
